1633f9de73449cdd19ad1499c27322c372ea87a4
Charme-Lee <charmelishivha2@gmail.com>
2025-09-14 16:02:23 +0200

    Add event listeners on load

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 import { podcasts, genres } from "./data.js";
 import { createModalController } from "./components/createModal.js";
 import { createGrid } from "./views/createGrid.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("data-container");
+  const modalController = createModalController();
+
+  createGrid(container, podcasts, genres, modalController);
+});
